776113e39a5602e3b39682dfc94d1b5beb4d50e8
unknown <seokungarna@gmail.com>
2020-09-15 18:40:47 +0200

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,9 @@
 import React from 'react'
-import InstagramFeed from './InstagramFeed'
 import './Footer.css'
 
 export default () => (
   <div>
-    {/* <h2 className="taCenter">
-      Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@tjay</a>
-    </h2>
-    <br /> */}
-    {/* <InstagramFeed count="8" /> */}
+   
     <footer className="footer">
       <div className="container taCenter">
         <span>
